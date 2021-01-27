b225680c63658935d218bc498029c9421a5a087e
K4YT3X <k4yt3x@k4yt3x.com>
2021-01-26 22:07:45 -0500

    added build commands into setup.py comments

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ Last Modified: January 11, 2021
 
 Dev: K4YT3X
 Last Modified: January 26, 2021
+
+pip3 install --user --U setuptools wheel twine
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository pypi dist/*
 """
 
 import setuptools
