e37a2399ce08b590d9b3aecc932fccaf4e209a06
K4YT3X <k4yt3x@k4yt3x.com>
2021-06-22 06:37:33 +0000

    updated init and main docs and modes

--- wg_meshconf/__init__.py
+++ wg_meshconf/__init__.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Name: wg-meshconf __init__
+Creator: K4YT3X
+Date Created: May 21, 2021
+Last Modified: June 22, 2021
+"""
+
 from .wg_meshconf import main
+
